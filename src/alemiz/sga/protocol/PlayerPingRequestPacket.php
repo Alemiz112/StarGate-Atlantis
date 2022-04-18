a0def3939c4bcb7f835736e6fe81ca9d3aec1bb8
Alemiz <alex@mizerak.eu>
2022-04-18 15:19:44 +0200

    Implement player ping packets

---
+++ src/alemiz/sga/protocol/PlayerPingRequestPacket.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Copyright 2022 Alemiz
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace alemiz\sga\protocol;
+
+use alemiz\sga\codec\StarGatePacketHandler;
+use alemiz\sga\codec\StarGatePackets;
+use alemiz\sga\protocol\types\PacketHelper;
+
+class PlayerPingRequestPacket extends StarGatePacket {
+
+    /** @var string */
+    private $playerName;
+
+    public function encodePayload() : void {
+        PacketHelper::writeString($this, $this->playerName);
+    }
+
+    public function decodePayload() : void {
+        $this->playerName = PacketHelper::readString($this);
+    }
+
+    /**
+     * @param StarGatePacketHandler $handler
+     * @return bool
+     */
+    public function handle(StarGatePacketHandler $handler) : bool {
+        return $handler->handlePlayerPingRequest($this);
+    }
+
+    public function getPacketId() : int {
+        return StarGatePackets::PLAYER_PING_REQUEST_PACKET;
+    }
+
+    /**
+     * @return bool
+     */
+    public function sendsResponse() : bool {
+        return true;
+    }
+
+    /**
+     * @param string $playerName
+     */
+    public function setPlayerName(string $playerName) : void {
+        $this->playerName = $playerName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlayerName() : string {
+        return $this->playerName;
+    }
+}
